52c5b24902b768697d17c3b7bc5f139343386cd6
DaveWebCode <maxgeoland@gmail.com>
2023-07-29 01:06:44 +0300

    home work of 1st week is done

--- projects/functions/index.js
+++ projects/functions/index.js
@@ -12,7 +12,9 @@
 
  Другими словами: функция должна возвращать в неизменном виде то, что поступает ей на вход
  */
-function returnFirstArgument() {}
+function returnFirstArgument(bob) {
+  return bob;
+};
 
 /*
  Задание 2:
@@ -28,7 +30,9 @@ function returnFirstArgument() {}
  Пример:
    sumWithDefaults(10) вернет 110
  */
-function sumWithDefaults() {}
+function sumWithDefaults(a = 5, b = 100) {
+  return a + b;
+};
 
 /*
  Задание 3:
@@ -38,7 +42,9 @@ function sumWithDefaults() {}
  Пример:
    returnFnResult(() => 'привет') вернет 'привет'
  */
-function returnFnResult() {}
+function returnFnResult(fn1) {
+  return fn1();
+};
 
 /*
  Задание 4:
@@ -53,7 +59,14 @@ function returnFnResult() {}
    console.log(f()); // выведет 12
    console.log(f()); // выведет 13
  */
-function returnCounter() {}
+
+function returnCounter(number = 0){
+  function f(){
+    return ++number;
+  }
+  return f;
+}
+
 
 /*
  Задание 5 *:
@@ -64,7 +77,15 @@ function returnCounter() {}
  Пример:
    returnArgumentsArray(1, 2, 3) вернет [1, 2, 3]
  */
-function returnArgumentsArray() {}
+function returnArgumentsArray() {
+  let result = [];
+
+  for (let i = 0; i < arguments.length; i++) {
+    result.push(arguments[i]);
+  }
+
+  return result;
+}; 
 
 export {
   returnFirstArgument,
