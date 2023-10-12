2bf5cf66ada5ea1d694173f86f6ef89d45a85d41
DaveWebCode <maxgeoland@gmail.com>
2023-10-12 18:40:33 +0300

    I did 4th week hometask

--- webpack.config.js
+++ webpack.config.js
@@ -65,6 +65,16 @@ module.exports = {
         use: [{ loader: './scripts/html-inject-loader.js' }, { loader: 'html-loader' }],
       },
       {
+        test: /\.html/,
+        include: [path.resolve(__dirname, 'projects')],
+        use: [
+          {loader: './scripts/html-inject-loader.js'},
+          {
+            loader: 'raw-loader',
+          },
+        ],
+      },      
+      {
         test: /\.(jpe?g|png|gif|svg|eot|ttf|woff|woff2)$/i,
         type: 'asset/resource',
       },
